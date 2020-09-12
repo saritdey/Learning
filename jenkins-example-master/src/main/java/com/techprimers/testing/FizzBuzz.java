f846502654f221ede40aa91f2638027abd9c12c4
saritdey <saritdey@yahoo.com>
2020-09-12 09:45:29 -0500

    after first build

--- jenkins-example-master/src/main/java/com/techprimers/testing/FizzBuzz.java
+++ jenkins-example-master/src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
 
-        System.out.println("Test");
+        System.out.println("Test2");
         return String.valueOf(number);
     }
 }
